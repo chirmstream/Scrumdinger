d3a36b7243fe4c90c09cbe2f3e28dc5d89a961e2
Dexter Barney <dextergarney@gmail.com>
2023-09-07 13:56:33 -0700

    Using stacks to arrange views

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Dexter Barney on 9/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
