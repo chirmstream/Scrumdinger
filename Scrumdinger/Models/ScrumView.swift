cc5f51d8f7ba24320b4557824c4e590671584073
chirmstream <dextergarney@gmail.com>
2023-09-13 13:54:59 -0700

    commit test

--- Scrumdinger/Models/ScrumView.swift
+++ Scrumdinger/Models/ScrumView.swift
@@ -11,7 +11,9 @@ struct ScrumView: View {
     let scrums: [DailyScrum]
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(scrums, id: \.title) {scrum in
+            CardView(scrum: scrum)
+        }
     }
 }
 
