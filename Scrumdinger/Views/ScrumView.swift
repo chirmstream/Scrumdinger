f6806e4beba990a3f14e5e7fecd1cfc12fb6ea5e
chirmstream <33538092+chirmstream@users.noreply.github.com>
2023-09-14 12:39:25 -0700

    Syntex issue fixed.
    Still not getting card background color

--- Scrumdinger/Views/ScrumView.swift
+++ Scrumdinger/Views/ScrumView.swift
@@ -7,19 +7,19 @@
 
 import SwiftUI
 
-struct ScrumView: View {
+struct ScrumsView: View {
     let scrums: [DailyScrum]
     
     var body: some View {
-        List(scrums, id: \.title) {scrum in
+        List(scrums, id: \.title) { scrum in
             CardView(scrum: scrum)
                 .listRowBackground(scrum.theme.mainColor)
         }
     }
 }
 
-struct ScrumView_Previews: PreviewProvider {
+struct ScrumsView_Previews: PreviewProvider {
     static var previews: some View {
-        ScrumView(scrums: DailyScrum.sampleData)
+        ScrumsView(scrums: DailyScrum.sampleData)
     }
 }
