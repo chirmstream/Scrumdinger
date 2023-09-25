7dba952f34db53ebfd0b6f27d1e57ee98d00c996
chirmstream <33538092+chirmstream@users.noreply.github.com>
2023-09-25 13:35:05 -0700

    Started Passing data with bindings.
    Step 2 complete.

---
+++ Scrumdinger/Views/ThemeView.swift
@@ -0,0 +1,21 @@
+//
+//  ThemeView.swift
+//  Scrumdinger
+//
+//  Created by Dexter Barney on 9/25/23.
+//
+
+import SwiftUI
+
+struct ThemeView: View {
+    let theme: Theme
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ThemeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemeView(theme: .buttercup)
+    }
+}
